7846eb7ae30a78f5a051d704df934083d696704c
Xu Desheng <8742037+xudesheng@users.noreply.github.com>
2019-06-15 11:41:27 -0400

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -3,6 +3,8 @@
 int main(int argc,char* argv[]){
     int distance=100;
     int travel_time=3;
+    
+    //test
     printf("You are %d mile away,it will take %d hour to reach there\n",distance,travel_time);
     printf("\tArgc is equal to %d, %s\n",argc, argv[1]);//%s print string
     return 0;
